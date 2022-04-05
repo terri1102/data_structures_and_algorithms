5245e4c9535dc9f4e2ffca959e336657d6a692ae
Terri <terricodes@gmail.com>
2022-04-05 22:02:01 +0900

    Time: 33 ms (87.23%), Space: 13.7 MB (99.71%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -2,7 +2,9 @@ from math import comb
 
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        "two sum이랑 같은 문제 아녀"
+        "two sum이랑 같은 문제 아녀????"
+        
+        
         
         hash_map = {}
         for i in range(len(nums)):
@@ -10,10 +12,10 @@ class Solution:
                 hash_map[nums[i]] = 0
             else:
                 hash_map[nums[i]] += 1
+        return sum(k * (k + 1) // 2 for k in hash_map.values())
+#         cnt = 0
+#         for d in hash_map.values():
+#             cnt += comb(d+1,2)
 
-        cnt = 0
-        for d in hash_map.values():
-            cnt += comb(d+1,2)
-
-        return cnt
+#         return cnt
         
